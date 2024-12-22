eb0afb08b6c89031d1260bcfd33982578fe43631
SaschaKohler <sascha.kohler@sent.at>
2024-12-22 11:03:02 +0100

    fix(chore): m-mode

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,5 @@
 // src/pages/Home.tsx
 import React from "react";
-import DefaultLayout from "../layouts/default/DefaultLayout";
-import Hero from "../components/common/Hero";
-import Services from "../components/sections/Services";
-import PricingPackages from "../components/sections/PricingPackages";
-import About from "../components/sections/About";
-import ContactForm from "../components/common/ContactForm";
 import MaintenancePage from "./Maintenance";
 
 const Home: React.FC = () => {
