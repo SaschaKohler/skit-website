e30128ad19be57d2518e29f1974e4545c2a847e6
SaschaKohler <sascha.kohler@sent.at>
2025-01-03 18:28:43 +0100

    feat(json+ld): sEO optimizing

--- src/layouts/default/DefaultLayout.tsx
+++ src/layouts/default/DefaultLayout.tsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import Header from './Header'
 import Footer from './Footer'
 import PageTransition from '../../components/common/PageTransition'
 import ScrollButton from '../../components/utils/ScrollToTopButton'
+
+const schemaData = {
+  '@context': 'https://schema.org',
+  '@type': 'ProfessionalService',
+  name: 'sk.IT - Sascha Kohler',
+  description: 'IT Dienstleistungen und Webentwicklung',
+  url: 'https://skit.sascha-kohler.at',
+  telephone: '+43(0)650 90 30 372',
+  email: 'office@sascha-kohler.at',
+  address: {
+    '@type': 'PostalAddress',
+    addressCountry: 'AT',
+  },
+  geo: {
+    '@type': 'GeoCoordinates',
+    latitude: '48.2082',
+    longitude: '16.3738',
+  },
+  sameAs: [
+    'https://www.linkedin.com/in/sascha-kohler',
+    'https://github.com/SaschaKohler',
+  ],
+}
 
 interface DefaultLayoutProps {
   children: React.ReactNode
@@ -10,14 +34,19 @@ interface DefaultLayoutProps {
 
 const DefaultLayout: React.FC<DefaultLayoutProps> = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="flex-grow">
-        <PageTransition>{children}</PageTransition>
-        <ScrollButton />
-      </main>
-      <Footer />
-    </div>
+    <>
+      <Helmet>
+        <script type="application/ld+json">{JSON.stringify(schemaData)}</script>
+      </Helmet>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">
+          <PageTransition>{children}</PageTransition>
+          <ScrollButton />
+        </main>
+        <Footer />
+      </div>
+    </>
   )
 }
 
